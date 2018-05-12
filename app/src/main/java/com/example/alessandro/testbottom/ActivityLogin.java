4c30c1cc4d7a953494521cefb78535be40018419
Alessandro <afoglie@cetys.edu.mx>
2018-05-11 23:59:21 -0700

    Conexión API Login y parsing respuesta

---
+++ app/src/main/java/com/example/alessandro/testbottom/ActivityLogin.java
@@ -0,0 +1,170 @@
+package com.example.alessandro.testbottom;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.pass);
+        submit = (Button)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Login(email.getText().toString(),password.getText().toString(),ActivityLogin.this).execute();
+            }
+        });
+    }
+
+    private class Login extends AsyncTask<Void,Void,Void> {
+
+        private String usr;
+        private String pass;
+        private Context c;
+        private ProgressDialog pd;
+        private String result;
+        private Boolean success = false;
+
+        public Login (String usr, String pass, Context c) {
+            this.usr = usr;
+            this.pass = pass;
+            this.c = c;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            pd = new ProgressDialog(c);
+            pd.setMessage("Logging In...");
+            pd.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            URL url = null;
+            HttpURLConnection conn = null;
+            byte[] postDataBytes = null;
+            Reader in = null;
+            try {
+                url = new URL("http://35.197.88.181/signin");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            Map<String, Object> params = new LinkedHashMap<>();
+            params.put("email",usr);
+            params.put("password",pass);
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String, Object> param : params.entrySet()) {
+                if (postData.length() != 0) postData.append('&');
+                try {
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                postData.append('=');
+                try {
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                postDataBytes = postData.toString().getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                conn.setRequestMethod("POST");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            try {
+                conn.getOutputStream().write(postDataBytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            StringBuilder sb = new StringBuilder();
+            if(in!=null) {
+            try {
+                for (int c; (c = in.read()) >= 0; )
+                    sb.append((char) c);
+                result = sb.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            success = true;
+            }
+            else {
+                // No se pudo hacer el login
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            String token = null;
+            pd.dismiss();
+            if(success) {
+                try {
+                    JSONObject mainObject = new JSONObject(result);
+                    token = mainObject.getString("token");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(c,MainActivity.class);
+                i.putExtra("Token", token);
+                startActivity(i);
+                finish();
+            }
+            super.onPostExecute(aVoid);
+        }
+    }
+
+}
--- app/src/main/java/com/example/alessandro/testbottom/MainActivity.java
+++ app/src/main/java/com/example/alessandro/testbottom/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.alessandro.testbottom;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,28 +38,38 @@ public class MainActivity extends AppCompatActivity {
     private FragmentEntregados fragmentEntregados;
     private FragmentFavoritos fragmentFavoritos;
     private BottomNavigationView bottom_nav;
+    private String Token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("OMMBC");
-        bottom_nav = (BottomNavigationView)findViewById(R.id.navigation);
-        main_content = (FrameLayout)findViewById(R.id.main_content);
+        Token = getIntent().getStringExtra("Token");
+        bottom_nav = (BottomNavigationView) findViewById(R.id.navigation);
+        main_content = (FrameLayout) findViewById(R.id.main_content);
         fragmentList = new FragmentList();
         fragmentEntregados = new FragmentEntregados();
         fragmentFavoritos = new FragmentFavoritos();
         setFragment(fragmentList);
+        Allertami(Token);
 
         bottom_nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.navigation_problemas:    setFragment(fragmentList); return true;
-                    case R.id.navigation_entregados:   setFragment(fragmentEntregados); return true;
-                    case R.id.navigation_favoritos:    setFragment(fragmentFavoritos); return true;
+                    case R.id.navigation_problemas:
+                        setFragment(fragmentList);
+                        return true;
+                    case R.id.navigation_entregados:
+                        setFragment(fragmentEntregados);
+                        return true;
+                    case R.id.navigation_favoritos:
+                        setFragment(fragmentFavoritos);
+                        return true;
 
-                    default: return false;
+                    default:
+                        return false;
                 }
             }
 
@@ -47,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 
     private void setFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -71,5 +102,126 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void Allertami(String msg) {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(this);
+        }
+        builder.setTitle("Token de sesión")
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    /*class callAPI extends AsyncTask<Void, Void, Void>  {
+
+        String response = "No response";
+        String usr;
+        String pwd;
+        Context mycont;
+        ProgressDialog dialog;
+
+
+        public callAPI(String usr, String pwd, Context mycont) {
+            this.usr = usr;
+            this.pwd = pwd;
+            this.mycont = mycont;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(mycont);
+            dialog.setMessage("Logging In");
+            dialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            URL url = null;
+            HttpURLConnection conn = null;
+            byte[] postDataBytes = null;
+            Reader in = null;
+            try {
+                url = new URL("http://35.197.88.181/signin");
+            } catch (MalformedURLException e) {
+                Allertami(e.getMessage());
+            }
+            Map<String, Object> params = new LinkedHashMap<>();
+            params.put("email", usr);
+            params.put("password", pwd);
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String, Object> param : params.entrySet()) {
+                if (postData.length() != 0) postData.append('&');
+                try {
+                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    Allertami(e.getMessage());
+                }
+                postData.append('=');
+                try {
+                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    Allertami(e.getMessage());
+                }
+            }
+            try {
+                postDataBytes = postData.toString().getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                Allertami(e.getMessage());
+            }
+            try {
+                conn = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                Allertami(e.getMessage());
+            }
+            try {
+                conn.setRequestMethod("POST");
+            } catch (ProtocolException e) {
+                Allertami(e.getMessage());
+            }
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            try {
+                conn.getOutputStream().write(postDataBytes);
+            } catch (IOException e) {
+                Allertami(e.getMessage());
+            }
+            try {
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } catch (IOException e) {
+                Allertami(e.getMessage());
+            }
+            StringBuilder sb = new StringBuilder();
+            try {
+                for (int c; (c = in.read()) >= 0; )
+                    sb.append((char) c);
+                 response = sb.toString();
+            } catch (IOException e) {Allertami(e.getMessage());}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            dialog.dismiss();
+        }
+    }*/
+
 
 }
