88580e6df8c8119ff4b33e1dac153dc2876e6e2a
Alessandro <afoglie@cetys.edu.mx>
2018-05-17 16:54:11 -0700

    Clase LaTex para almacenar String LaTex

---
+++ app/src/main/java/com/example/alessandro/testbottom/LaTex.java
@@ -0,0 +1,14 @@
+package com.example.alessandro.testbottom;
+
+public class LaTex {
+
+    private String text;
+
+    public LaTex(String text) {
+        this.text = text;
+    }
+
+    public void convert() {
+        String convertita = "";
+    }
+}
